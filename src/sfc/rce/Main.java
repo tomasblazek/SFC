bb3f57c8a6f65b4d0041f46b38868813603d18a7
Unknown <tblazek33@gmail.com>
2019-10-19 18:16:10 +0200

    Create demo training set

--- src/sfc/rce/Main.java
+++ src/sfc/rce/Main.java
@@ -1,9 +1,28 @@
 package sfc.rce;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Ola amigo");
+
+        ArrayList<Integer> inputVector;
+        ArrayList<ArrayList<Integer>> trainingSet = new ArrayList<ArrayList<Integer>>();
+
+        for (int a = 0; a < 3; a++){
+            inputVector = new ArrayList<Integer>();
+            for (int i = 0; i < 5; i++) {
+                inputVector.add(a);
+            }
+            trainingSet.add(inputVector);
+        }
+        System.out.println(trainingSet);
+
+
+
+
+
     }
 }
