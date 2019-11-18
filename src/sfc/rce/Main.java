b34c6ae354b7d69e2d4dcd66203ea31fb59544e2
Unknown <tblazek33@gmail.com>
2019-11-18 23:10:48 +0100

    Refactor of Printing and Argument parsing

--- src/sfc/rce/Main.java
+++ src/sfc/rce/Main.java
@@ -11,42 +11,11 @@ import java.util.NoSuchElementException;
 
 public class Main {
 
-    private static void argParse(String[] args){
-        for (int i = 0; i < args.length; i++){
-            if (i + 1 == args.length){
-                System.err.print("Error: Missing value for " + args[i] + "\n");
-                System.exit(1);
-            }
-
-            try {
-                switch (args[i]) {
-                    case "-t":
-                        Arguments.trainingFile = args[i + 1];
-                        break;
-                    case "-v":
-                        Arguments.validationFile = args[i + 1];
-                        break;
-                    case "-R":
-                        Arguments.maxRadius = Double.parseDouble(args[i + 1]);
-                        break;
-                    case "-r":
-                        Arguments.decreaseRation = Double.parseDouble(args[i + 1]);
-                        break;
-                }
-                i++;
-            }
-             catch (NumberFormatException e) {
-                System.err.println("Error: Invalid value of " + args[i] + "! Should be floating point number.\n");
-                System.exit(1);
-            }
-        }
-    }
-
     public static void main(String[] args) {
         String data = null;
         Dataset dataset = null;
 
-        argParse(args);
+        Arguments.argParse(args);
 
         Net net = new Net(Arguments.maxRadius);
 
@@ -96,10 +65,7 @@ public class Main {
                     System.exit(0);
                 }
 
-                if (result == null){
-                    result = "Unknown";
-                }
-                System.out.print("Result: " + result + "\n\n");
+                Printer.printResult(result);
             }
         }
     }
